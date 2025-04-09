e6666f37cf45a586c945076e564a895561911804
fordgoat <adaadaajapan@gmail.com>
2025-04-09 14:41:43 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -130,14 +130,8 @@ async def not_joined(client: Client, message: Message):
         ]
         [
             InlineKeyboardButton(text="JOIN 3", url=client.invitelink3),
-        ]
-        [
             InlineKeyboardButton(text="JOIN 4", url=client.invitelink4),
-        ]
-        [
             InlineKeyboardButton(text="JOIN 5", url=client.invitelink5),
-        ]
-        [
             InlineKeyboardButton(text="JOIN 6", url=client.invitelink6),
         ],
     ]
